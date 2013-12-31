881dc93ca2df2f9b56af33f31d31c643a838f606
Aman Mangal <mangalaman93@gmail.com>
2013-12-31 21:13:48 +0530

    push command working

--- rlib/spreadsheet.rb
+++ rlib/spreadsheet.rb
@@ -1,11 +1,50 @@
 require 'google_drive'
 
 class Spreadsheet
-	def initialize(email, password, key)
+	PHASES = 144
+
+	def initialize(email, password, key, name, id)
 		@session = GoogleDrive.login(email, password)
-		@ws = @session.spreadsheet_by_key(key).add_worksheet("test")
-		@row = 0; @col = 0
-		print "created a new worksheet!\n"
+		if id.nil?
+			@ws = @session.spreadsheet_by_key(key).worksheets[@session.spreadsheet_by_key(key).worksheets.size-1]
+		else
+			@ws = @session.spreadsheet_by_key(key).worksheets[id]
+		end
+		@row = 1; @col = 1;
+	end
+
+	def update_workloads(filename="input")
+		set_col(2); set_row(2);
+		begin
+			file = File.new(filename, "r")
+		    counter = 0; file.gets; file.gets; # skipping first 2 lines
+			while (line = file.gets) && counter < PHASES
+				tokens = line.split(" ")
+				store_next_row(tokens)
+				counter = counter + 1
+			end
+			save
+			file.close
+			puts "updated workload!"
+		rescue => err
+			puts "Exception occurred: #{err}"
+		end
+	end
+
+	def update_utilizations(filename="results/mdp_util.txt")
+		set_col(15); set_row(2);
+		begin
+			file = File.new(filename, "r")
+			while (line = file.gets)
+				tokens = line.split(" ")
+				store_next_row(tokens)
+			end
+			save
+			file.close
+			puts "updated utilizations!"
+		rescue => err
+			puts "Exception occurred: #{err}"
+		end
 	end
 
 	def store_next_row(vals)
@@ -20,6 +59,11 @@ class Spreadsheet
 			@ws[@row+row, @col] = val
 		}
 		@col = @col + 1
+	end
+
+	def save
+		@ws.save
+		@ws.reload
 	end
 
 	def set_col(col)
