7b348f3ee0829405b72f61227a06927d283c1f99
Aman Mangal <mangalaman93@gmail.com>
2014-03-20 05:04:51 +0530

    Added timing info in output

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sys/time.h>
 #include "simdata.h"
 #include "khanna.h"
 #include "mdp.h"
@@ -13,24 +14,34 @@ int main()
     SimData *sdata = new SimData(num_vms, num_phases);
     sdata->readInput();
 
+    struct timeval t_begin, t_end;
+    gettimeofday(&t_begin, NULL);
     cout<<"############## mdp ##############"<<endl;
     Mdp mdp(sdata);
     mdp.run(num_phases);
     mdp.printPolicy();
     mdp.dumpPolicy();
+    gettimeofday(&t_end, NULL);
+    cout<<"Time Taken: "<<(t_end.tv_sec-t_begin.tv_sec)*1000000 + t_end.tv_usec-t_begin.tv_usec<<endl;
 
+    gettimeofday(&t_begin, NULL);
     cout<<"############## astar ##############"<<endl;
     Astar astar(sdata);
     astar.run(num_phases);
     astar.printPolicy();
     astar.dumpPolicy();
+    gettimeofday(&t_end, NULL);
+    cout<<"Time Taken: "<<(t_end.tv_sec-t_begin.tv_sec)*1000000 + t_end.tv_usec-t_begin.tv_usec<<endl;
 
+    gettimeofday(&t_begin, NULL);
     cout<<endl;
     cout<<"############## khanna ##############"<<endl;
     Khanna kh(sdata);
     kh.run(num_phases);
     kh.printPolicy();
     kh.dumpPolicy();
+    gettimeofday(&t_end, NULL);
+    cout<<"Time Taken: "<<(t_end.tv_sec-t_begin.tv_sec)*1000000 + t_end.tv_usec-t_begin.tv_usec<<endl;
 
     delete sdata;
 	return 0;
